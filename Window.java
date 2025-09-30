439b1d1fc5d5b287fe47d4449f5a7786dea303ad
Mr. Bird <pc.racidae@gmail.com>
2025-10-01 01:54:08 +0800

    Update Window.java
    Made the window stuff a little bit better.

--- Window.java
+++ Window.java
@@ -1,21 +1,64 @@
 import java.awt.*;
 import javax.swing.*;
 
-public class Window extends JFrame {
+public class Window extends JPanel {
 	
-	Panel mainPanel;
+	JFrame frame;
+	
+	static final int WIDTH 	= 500;
+	static final int HEIGHT = 500;
+	
+	public int x1 = 500;
 	
 	Window(){
 		
-		mainPanel = new Panel();
+		frame = new JFrame();
 		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.add(mainPanel);
-		this.pack();
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-		this.setTitle("Fourier Transform");
+		this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setResizable(false);
+		frame.setUndecorated(true);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		
+		
+		
+		frame.add(this);
+		frame.pack();
+	}
+	
+	public void paint(Graphics g) {
+		
+		setBackground(Color.WHITE);
+		
+		Graphics2D g2D = (Graphics2D) g;
+		Shape.drawLine(
+		0, 		0, 
+		x1, 	500, 
+		g2D, 	1);
+		
+		Shape.drawLine(
+		500, 	0, 
+		0, 		500, 
+		g2D, 	1);
+		
+		//g2D.drawOval(0, 0, 500, 500);
+		
+		System.out.println("Reprinting");
+		try {
+			
+			Thread.sleep(1000);
+			
+			
+		} catch (Exception e) {
+			
+		}
+		x1--;
+		repaint();
 		
 	}
+	
 	
 }
